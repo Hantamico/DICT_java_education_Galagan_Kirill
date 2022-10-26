df68007ede8642e712eb7d556f35995d5d3bca33
Kirill <galagan.k.a.01@gmail.com>
2022-10-26 19:02:24 +0300

    hangman final stage

--- src/Hangman/HangmanTest.java
+++ src/Hangman/HangmanTest.java
@@ -1,9 +1,12 @@
 package Hangman;
 
+import java.util.Scanner;
+
 public class HangmanTest {
     public static void main(String[] args) {
+        Scanner user = new Scanner(System.in);
         Hangman hangman = new Hangman();
-        hangman.anons();
-        hangman.guessWord();
+        hangmanMenu menu = new hangmanMenu();
+        menu.hangmanMenu(user, hangman);
     }
 }
