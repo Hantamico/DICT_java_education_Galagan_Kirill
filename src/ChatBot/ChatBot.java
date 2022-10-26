8d095f514c8401a26ac48f93aa21e468b3840064
Kirill <galagan.k.a.01@gmail.com>
2022-10-26 16:59:05 +0300

    chatbot 4-th stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -16,6 +16,11 @@ public class ChatBot {
             int rem7 = scanner.nextInt() % 7;
             int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
             System.out.println("Your age is " + age + "; that`s a good time to start programming!");
+            System.out.println("Now I will prove to you that I can  count to any number you want");
+            int number = scanner.nextInt();
+            for (int i = 0; i <= number; i++) {
+                System.out.println(i + "!");
+            }
         }
     }
 }
