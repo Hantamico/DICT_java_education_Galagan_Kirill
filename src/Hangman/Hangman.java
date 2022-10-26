e83413e115bb68cd4273375235b13eb17267fc49
Kirill <galagan.k.a.01@gmail.com>
2022-10-26 18:06:53 +0300

    hangman stage 1

---
+++ src/Hangman/Hangman.java
@@ -0,0 +1,8 @@
+package Hangman;
+
+public class Hangman {
+    public void anons() {
+        System.out.println("HANGMAN");
+        System.out.println("The game will be available soon.");
+    }
+}
---
+++ src/Hangman/HangmanTest.java
@@ -0,0 +1,8 @@
+package Hangman;
+
+public class HangmanTest {
+    public static void main(String[] args) {
+        Hangman hangmen = new Hangman();
+        hangmen.anons();
+    }
+}
