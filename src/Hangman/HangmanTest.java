e47dee7b4e9fd25f43252c83b9c48b470c31cb4a
Kirill <galagan.k.a.01@gmail.com>
2022-10-26 18:20:54 +0300

    hangman stage 2

--- src/Hangman/HangmanTest.java
+++ src/Hangman/HangmanTest.java
@@ -2,7 +2,8 @@ package Hangman;
 
 public class HangmanTest {
     public static void main(String[] args) {
-        Hangman hangmen = new Hangman();
-        hangmen.anons();
+        Hangman hangman = new Hangman();
+        hangman.anons();
+        hangman.guessWord();
     }
 }
