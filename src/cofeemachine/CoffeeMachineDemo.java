930a3c39e92b28084cb0e3ad09f64dbefea2816f
Kirill_Halahan <Galagan.k.a.01@gmail.com>
2022-12-03 14:28:04 +0200

    Coffee Machine 2-nd stage

--- src/cofeemachine/CoffeeMachineDemo.java
+++ src/cofeemachine/CoffeeMachineDemo.java
@@ -1,8 +1,10 @@
 package coffeeMachine;
 
+
+
 public class coffeeMachineDemo {
     public static void main(String[] args) {
         coffeeMachine coffeeMachine = new CoffeeMachine();
-        coffeeMachine.printText();
+        coffeeMachine.amountCoffee();
     }
 }
