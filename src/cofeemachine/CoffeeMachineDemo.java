298e8552b1cd27ab7e89c494b80f2ac77da0fde1
Kirill_Halahan <Galagan.k.a.01@gmail.com>
2022-12-03 14:39:20 +0200

    Coffee Machine 3-d stage

--- src/cofeemachine/CoffeeMachineDemo.java
+++ src/cofeemachine/CoffeeMachineDemo.java
@@ -6,5 +6,6 @@ public class coffeeMachineDemo {
     public static void main(String[] args) {
         coffeeMachine coffeeMachine = new CoffeeMachine();
         coffeeMachine.amountCoffee();
+        coffeeMachine.amountCups();
     }
 }
