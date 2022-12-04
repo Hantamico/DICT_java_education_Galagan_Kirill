d003eb2a5101d167e67dadad9b303d65df159e98
Kirill_Halahan <Galagan.k.a.01@gmail.com>
2022-12-04 10:41:24 +0200

    Coffee Machine 5-th stage

--- src/cofeemachine/CoffeeMachineDemo.java
+++ src/cofeemachine/CoffeeMachineDemo.java
@@ -1,11 +1,10 @@
 package coffeeMachine;
 
-
+import java.util.concurrent.atomic.AtomicLong;
 
 public class coffeeMachineDemo {
     public static void main(String[] args) {
         coffeeMachine coffeeMachine = new CoffeeMachine();
-        coffeeMachine.printInfo();
         coffeeMachine.menu();
     }
 }
